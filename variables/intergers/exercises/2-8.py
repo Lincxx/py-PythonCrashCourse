154d178c20c327ac8e1b28b899adec65846a508a
Jeremy Lincoln <bossco28@gmail.com>
2017-06-07 12:30:02 -0400

    Interger exercises completed

---
+++ variables/intergers/exercises/2-8.py
@@ -0,0 +1,14 @@
+# 2-8. Number Eight: Write addition, subtraction, multiplication, and division
+# operations that each result in the number 8. Be sure to enclose your operations
+# in print statements to see the results. You should create four lines that look
+# like this: print(5 + 3)
+
+print(5 + 3)
+print(108 - 100)
+print(1 * 8)
+print(80 / 10)
+
+
+
+
+
---
+++ variables/intergers/exercises/2-9.py
@@ -0,0 +1,10 @@
+# 2-9. Favorite Number: Store your favorite number in a variable. Then, using
+# that variable, create a message that reveals your favorite number. Print that
+# message.
+
+
+fav_num = 33
+
+message = "This is my favorite number: " + str(fav_num) + ". For no real reason"
+
+print(message)
