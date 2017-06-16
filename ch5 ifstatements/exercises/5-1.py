e7236f16d93b4af488628ff5c9146b0159e75afd
Jeremy Lincoln <bossco28@gmail.com>
2017-06-16 14:47:07 -0400

    Exercises 5-1 - 5-2 completed

---
+++ ch5 ifstatements/exercises/5-1.py
@@ -0,0 +1,34 @@
+# 5-1. Conditional Tests: Write a series of conditional tests. Print a statement
+# describing each test and your prediction for the results of each test. Your code
+# should look something like this:
+# car = 'subaru'
+# print("Is car == 'subaru'? I predict True.")
+# print(car == 'subaru')
+# print("\nIs car == 'audi'? I predict False.")
+# print(car == 'audi')
+# • Look closely at your results, and make sure you understand why each line
+# evaluates to True or False.
+# • Create at least 10 tests. Have at least 5 tests evaluate to True and another
+# 5 tests evaluate to False.
+
+fruit = 'orange'
+
+print('Is "strawberry" == fruit? I predict False')
+print('strawberry' == fruit)
+
+vehicle = 'jeep'
+print('Is "jeep" == vehicle? I predict True ')
+print(vehicle == 'jeep')
+
+print(1 == 1)
+print(2 != 3)
+print('h' == 'h')
+print(2 < 4)
+print(3 <= 3)
+
+print(1 != 1)
+print(2 == 3)
+print('h' == 'g')
+print(2 > 4)
+print(3 < 3)
+
---
+++ ch5 ifstatements/toppings.py
@@ -0,0 +1,6 @@
+# Checking for inequality
+
+requested_toppings = 'mushrooms'
+
+if requested_toppings != 'anchovies':
+    print("Hold the anchovies")
