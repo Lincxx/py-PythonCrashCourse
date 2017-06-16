80c401d2afea5d4eaca5fa4279d2787994efdbd6
Jeremy Lincoln <bossco28@gmail.com>
2017-06-16 16:08:23 -0400

    Exercises 5-3 - 5-7 completed

---
+++ ch5 ifstatements/exercises/5-7.py
@@ -0,0 +1,19 @@
+# 5-7. Favorite Fruit: Make a list of your favorite fruits, and then write a series of
+# independent if statements that check for certain fruits in your list.
+# • Make a list of your three favorite fruits and call it favorite_fruits.
+# • Write five if statements. Each should check whether a certain kind of fruit
+# is in your list. If the fruit is in your list, the if block should print a statement,
+# such as You really like bananas!
+
+fruits = ['banana', 'watermelon', 'kiwi', 'mango']
+
+if 'banana' in fruits:
+    print('You really like banana')
+if 'strawberry' in fruits:
+    print('You really like strawberry')
+if 'kiwi' in fruits:
+    print('You really like kiwi')
+if 'mango' in fruits:
+    print('You really like mango')
+if 'dates' in fruits:
+    print('You really like dates')
--- ch5 ifstatements/toppings.py
+++ ch5 ifstatements/toppings.py
@@ -1,6 +1,17 @@
 # Checking for inequality
 
-requested_toppings = 'mushrooms'
+# requested_toppings = 'mushrooms'
+#
+# if requested_toppings != 'anchovies':
+#     print("Hold the anchovies")
 
-if requested_toppings != 'anchovies':
-    print("Hold the anchovies")
+requested_toppings = ['mushrooms', 'extra cheese']
+
+if 'mushrooms' in requested_toppings:
+    print("Adding mushrooms")
+if 'pepperoni' in requested_toppings:
+    print("Adding pepperoni")
+if 'extra cheese' in requested_toppings:
+    print("Adding extra cheese")
+
+print("\nFinished making your pizza!")
